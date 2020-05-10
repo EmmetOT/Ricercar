848d9d6d247f51c11e3d45434f6ba1cb4c4e761f
Emmet <emmettot@gmail.com>
2020-05-11 00:50:57 +0100

    Improved Gravity Visualization

--- Ricercar/Assets/Scripts/Gravity/GravityVisualizer.cs
+++ Ricercar/Assets/Scripts/Gravity/GravityVisualizer.cs
@@ -38,21 +38,25 @@ namespace Ricercar.Gravity
         {
             if (!m_showField)
                 return;
-
-            Vector2 topRight = m_camera.ViewportToWorldPoint(new Vector3(1f, 1f, 10f));
+            
             Vector2 bottomLeft = m_camera.ViewportToWorldPoint(new Vector3(0f, 0f, 10f));
+            Vector2 topLeft = m_camera.ViewportToWorldPoint(new Vector3(0f, 1f, 10f));
+            Vector2 bottomRight = m_camera.ViewportToWorldPoint(new Vector3(1f, 0f, 10f));
+            
+            Vector2 xComponent = Vector2.Lerp(bottomLeft, bottomRight, 1f / (m_fieldResolution - 1f)) - bottomLeft;
+            Vector2 yComponent = Vector2.Lerp(bottomLeft, topLeft, 1f / (m_fieldResolution - 1f)) - bottomLeft;
 
             for (int x = 0; x < m_fieldResolution; x++)
             {
                 for (int y = 0; y < m_fieldResolution; y++)
                 {
-                    Vector2 pos = new Vector3(Mathf.Lerp(bottomLeft.x, topRight.x, x / (m_fieldResolution - 1f)), Mathf.Lerp(bottomLeft.y, topRight.y, y / (m_fieldResolution - 1f)));
+                    Vector2 pos = bottomLeft + xComponent * x + yComponent * y;
 
                     Vector2 attraction = Attractor.GetGravityAtPoint(pos, 1f);
 
                     float attractionMagnitude = Mathf.Clamp(attraction.magnitude * m_vectorScale, m_minVectorMagnitude, m_maxVectorMagnitude);
                     attraction = attraction.normalized * attractionMagnitude;
-                    
+
                     Gizmos.color = Color.white;
                     Gizmos.DrawLine(pos, pos + attraction * m_vectorScale);
                 }
